ec156dedec6c363d8edb99274539a41ef62646b5
lindzht <llindsayttaylor@gmail.com>
2022-02-06 15:25:26 -0500

    added function to check whether guess is a single letter, and push to array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const buttonGuess = document.querySelector(".guess");
 const letter = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -7,6 +7,7 @@ const remainingSpan = document.querySelector("span");
 const message = document.querySelector(".message");
 const buttonPlayAgain = document.querySelector(".play-again hide");
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function(word){
     const placeholderLetters = [];
@@ -21,7 +22,36 @@ placeholder(word);
 
 buttonGuess.addEventListener("click", function(e){
     e.preventDefault();
+    message.innerText = "";
     const guess = letter.value;
     console.log(guess);
     letter.value = "";
+    const goodGuess = checkInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
 });
+
+const checkInput = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0){
+        message.innerText = `Please enter a letter`;
+    } else if (input.length > 1) {
+        message.innerText = `Please only enter one letter at a time`;
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = `Please enter only a letter from A-Z`;
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function(guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = `You've already guessed that letter silly goose. Try again`;
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+}
