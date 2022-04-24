5ffa370e2515762e0b2f0c796343cc594926ded2
lindzht <llindsayttaylor@gmail.com>
2022-04-24 13:39:33 -0400

    resetarted projected. added placeholder emojis and capturing input to form

--- js/script.js
+++ js/script.js
@@ -1,57 +1,88 @@
 const guessedLettersElement = document.querySelector(".guessed-letters");
 const buttonGuess = document.querySelector(".guess");
-const letter = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-const remaining = document.querySelector(".remaining");
+const remainingGuess = document.querySelector(".remaining");
 const remainingSpan = document.querySelector("span");
 const message = document.querySelector(".message");
 const buttonPlayAgain = document.querySelector(".play-again hide");
 const word = "magnolia";
-const guessedLetters = [];
 
-const placeholder = function(word){
+
+
+const updateWordInProgress  = function(word){
     const placeholderLetters = [];
-    for (const letter of word) {
-        console.log(letter);
+    for (let letter of word){
         placeholderLetters.push("●");
     }
     wordInProgress.innerText = placeholderLetters.join("");
-}
+};
 
-placeholder(word);
+updateWordInProgress(word);
 
 buttonGuess.addEventListener("click", function(e){
     e.preventDefault();
-    message.innerText = "";
-    const guess = letter.value;
-    console.log(guess);
-    letter.value = "";
-    const goodGuess = checkInput(guess);
+    const input = letterInput.value;
+    console.log(input);
+    letterInput.value = "";
+})
 
-    if (goodGuess) {
-        makeGuess(guess);
-    }
-});
 
-const checkInput = function(input){
-    const acceptedLetter = /[a-zA-Z]/;
-    if (input.length === 0){
-        message.innerText = `Please enter a letter`;
-    } else if (input.length > 1) {
-        message.innerText = `Please only enter one letter at a time`;
-    } else if (!input.match(acceptedLetter)) {
-        message.innerText = `Please enter only a letter from A-Z`;
-    } else {
-        return input;
-    }
-};
 
-const makeGuess = function(guess) {
-    guess = guess.toUpperCase();
-    if (guessedLetters.includes(guess)) {
-        message.innerText = `You've already guessed that letter silly goose. Try again`;
-    } else {
-        guessedLetters.push(guess);
-        console.log(guessedLetters);
-    }
-}
+
+
+
+
+
+
+
+
+
+// const guessedLetters = [];
+
+// const placeholder = function(word){
+//     const placeholderLetters = [];
+//     for (const letter of word) {
+//         console.log(letter);
+//         placeholderLetters.push("●");
+//     }
+//     wordInProgress.innerText = placeholderLetters.join("");
+// }
+
+// placeholder(word);
+
+// buttonGuess.addEventListener("click", function(e){
+//     e.preventDefault();
+//     message.innerText = "";
+//     const guess = letter.value;
+//     console.log(guess);
+//     letter.value = "";
+//     const goodGuess = checkInput(guess);
+
+//     if (goodGuess) {
+//         makeGuess(guess);
+//     }
+// });
+
+// const checkInput = function(input){
+//     const acceptedLetter = /[a-zA-Z]/;
+//     if (input.length === 0){
+//         message.innerText = `Please enter a letter`;
+//     } else if (input.length > 1) {
+//         message.innerText = `Please only enter one letter at a time`;
+//     } else if (!input.match(acceptedLetter)) {
+//         message.innerText = `Please enter only a letter from A-Z`;
+//     } else {
+//         return input;
+//     }
+// };
+
+// const makeGuess = function(guess) {
+//     guess = guess.toUpperCase();
+//     if (guessedLetters.includes(guess)) {
+//         message.innerText = `You've already guessed that letter silly goose. Try again`;
+//     } else {
+//         guessedLetters.push(guess);
+//         console.log(guessedLetters);
+//     }
+// }
